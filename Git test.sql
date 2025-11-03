bbe48730fc9cf85c9bebc2a19b27a0d54ab10852
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2025-11-03 10:49:57 +0200

    Delete Git test.sql

--- Git test.sql
+++
@@ -1,9 +0,0 @@
-USE GIT_HUB_DB;
-
-CREATE OR REPLACE TABLE orders (
-    order_id VARCHAR(10),
-    product_name VARCHAR(40),
-    order_value INTEGER,
-    product_price INTEGER,
-    order_date TIMESTAMP_NTZ
-);
